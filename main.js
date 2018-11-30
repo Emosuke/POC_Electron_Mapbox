ed3126597ff4a458668d6abfe381e154cdbc4803
infinity <infinity@localhost.localdomain>
2018-11-30 11:56:42 +0100

    ADD (main.js) : implémentation en cours du github benchmark w/ headless electron

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+apboxgl.accessToken = '';
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+console.log('Version: ' + mapboxgl.version);
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+const container = document.createElement('div');
+container.style.width = '1200px';
+container.style.height = '700px';
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+document.body.appendChild(container);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+const map = new mapboxgl.Map({
+    container: container,
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-77.01866, 38.888],
+    zoom: 13
+});
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
+map.on('load', () => {
+    map.repaint = true;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+    setTimeout(() => {
+        const _render = mapboxgl.Map.prototype._render;
+        const durations = [];
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+        map._render = () => {
+            const now = performance.now();
+            _render.call(map);
+            durations.push(performance.now() - now);
+        };
+        setTimeout(() => {
+            map.repaint = false;
+            durations.sort((a, b) => a - b);
+            console.log('Median frame: ' + Math.round(100 * durations[Math.floor(durations.length / 2)]) / 100);
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+            window.close();
+        }, 5000);
+    }, 1000);
+});
